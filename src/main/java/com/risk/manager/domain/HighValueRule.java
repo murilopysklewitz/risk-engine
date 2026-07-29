bd80dacad2088e74e1288d05c6ad023f489dacc3
Fuzikawa <murilopyskfuzikawa@gmail.com>
2026-07-29 11:31:50 -0300

    (feat) Simple HighValueRule

--- src/main/java/com/risk/manager/domain/HighValueRule.java
+++ src/main/java/com/risk/manager/domain/HighValueRule.java
@@ -6,6 +6,8 @@ import com.risk.manager.infra.api.TransactionCompletedDTO;
 import com.risk.manager.infra.persistence.TransactionMirror;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,22 @@ public class HighValueRule implements AssessmentRules {
     }
 
     @Override
-    public RiskResult assess(TransactionCompletedDTO transaction) {
+    public RiskAssessment assess(TransactionCompletedDTO transaction) {
         List<TransactionMirror> transactions = transactionMirrorRepository.findTopXBySourceUserIdOrderByCreatedAtDesc(transaction.sourceUserId(),20);
+        BigDecimal sum = BigDecimal.ZERO;
+        for(TransactionMirror t : transactions){
+            sum = sum.add(t.getAmount());
+        }
+        BigDecimal average = sum.divide(transaction.amount(), RoundingMode.HALF_DOWN);
+
+        int scoreCalculator =transaction.amount().divide(average, RoundingMode.HALF_DOWN).intValue();
+        int score = 0;
+        if ( scoreCalculator >= 2 && scoreCalculator < 2.99){
+            score = 15;
+        }else if( scoreCalculator >= 3 && scoreCalculator < 4){
+            score = 35;
+        }
+
+        return RiskAssessment.create(score, List.of("HIGH_VALUE_RULE"));
     }
 }
--- src/main/java/com/risk/manager/domain/ports/AssessmentRules.java
+++ src/main/java/com/risk/manager/domain/ports/AssessmentRules.java
@@ -1,8 +1,9 @@
 package com.risk.manager.domain.ports;
 
+import com.risk.manager.domain.RiskAssessment;
 import com.risk.manager.domain.RiskResult;
 import com.risk.manager.infra.api.TransactionCompletedDTO;
 
 public interface AssessmentRules {
-    RiskResult assess(TransactionCompletedDTO transaction);
+    RiskAssessment assess(TransactionCompletedDTO transaction);
 }
